da224c91630b0c07d41abe267ef3bc4d8ad75632
Xayah <zds1249475336@gmail.com>
2024-07-03 13:11:09 +0800

    Add reproducible flags

--- busybox.mk
+++ busybox.mk
@@ -9,6 +9,6 @@ LOCAL_DISABLE_FORMAT_STRING_CHECKS := true
 LOCAL_LDFLAGS := -static -Wl,--wrap=realpath
 LOCAL_CFLAGS := \
 -w -include include/autoconf.h -D__USE_BSD -D__USE_GNU \
--DBB_VER=\"$(BB_VER)\" -DBB_BT=AUTOCONF_TIMESTAMP
+-DBB_VER=\"$(BB_VER)\"
 
 LOCAL_SRC_FILES := \
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,7 +1,10 @@
+LOCAL_PATH := $(LOCAL_PATH)
+FILE_PREFIX_MAP := $(FILE_PREFIX_MAP)
+
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
 APP_PLATFORM := android-26
-APP_CFLAGS := -Wall -Oz -fomit-frame-pointer -flto
-APP_LDFLAGS := -flto
+APP_CFLAGS := -Wall -Oz -fomit-frame-pointer -flto -ffile-prefix-map=$LOCAL_PATH=$FILE_PREFIX_MAP
+APP_LDFLAGS := -flto -Wl,--gc-sections -Wl,--build-id=none -Wl,--hash-style=both
 
 ifeq ($(OS),Windows_NT)
 APP_SHORT_COMMANDS := true
